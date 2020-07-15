1ee536558d8f7b2cd64bd0aca1e2d9093b62857b
Emil Ståhl <47429161+emilstahl97@users.noreply.github.com>
2020-07-15 19:17:16 +0200

    Added sound effects

--- pong.py
+++ pong.py
@@ -1,4 +1,5 @@
 import turtle
+import os
 
 wn = turtle.Screen()
 wn.title("Pong by @emilstahl97")
@@ -42,6 +43,8 @@ ball.goto(0, 0)
 ball.dx = 2
 ball.dy = 2
 
+# Display scoring
+
 pen = turtle.Turtle()
 pen.speed(0)
 pen.color("white")
@@ -49,8 +52,6 @@ pen.penup()
 pen.hideturtle()
 pen.goto(0, 260)
 pen.write(f"Player A: {score_a}  Player B: {score_b}", align="center", font=("Courier", 24, "normal"))
-
-#move paddle a up
 
 def paddle_a_up():
     paddle_a.sety(paddle_a.ycor() + 20)
@@ -66,7 +67,6 @@ def paddle_b_up():
 def paddle_b_down():
     paddle_b.sety(paddle_b.ycor() - 20)
 
-
 #keyboard binding
 
 wn.listen()
@@ -76,7 +76,6 @@ wn.onkeypress(paddle_b_up, "Up")
 wn.onkeypress(paddle_b_down, "Down")
 
 #main game loop 
-
 
 while True:
     wn.update()
@@ -89,10 +88,12 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
+        os.system("afplay bounce.wav&")
 
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
+        os.system("afplay bounce.wav&")
 
     if ball.xcor() > 390:
         ball.goto(0, 0)
@@ -111,8 +112,9 @@ while True:
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50):
         ball.setx(340)
         ball.dx *= -1
+        os.system("afplay bounce.wav&")
 
     if (ball.xcor() < -340 and ball.xcor() > - 350) and (ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50):
         ball.setx(-340)
         ball.dx *= -1
-        
+        os.system("afplay bounce.wav&")
