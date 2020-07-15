10aea6a88dc9347efc6e4b7eeea8171fc06eea7f
Emil Ståhl <47429161+emilstahl97@users.noreply.github.com>
2020-07-15 17:15:03 +0200

    created ball

--- pong.py
+++ pong.py
@@ -16,6 +16,24 @@ paddle_a.color("white")
 paddle_a.penup()
 paddle_a.goto(-350, 0)
 
+#paddle b
+
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.color("white")
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+#ball
+
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
 
 #main game loop 
 
