52242edb6949e0474a3f4c5da8a0528787c43852
Emil Ståhl <47429161+emilstahl97@users.noreply.github.com>
2020-07-15 17:43:45 +0200

    ball moves

--- pong.py
+++ pong.py
@@ -34,6 +34,8 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 2
+ball.dy = 2
 
 #move paddle a up
 
@@ -64,3 +66,8 @@ wn.onkeypress(paddle_b_down, "Down")
 
 while True:
     wn.update()
+
+    #Move the ball
+
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
